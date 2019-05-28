86c88e7697a59cdd4260461b035e3024671328aa
sourav1547 <souravdas1547@gmail.com>
2019-05-28 19:11:21 +0530

    re-organization

--- downloadEVDCode.sh
+++ downloadEVDCode.sh
@@ -2,13 +2,17 @@
 
 cd gitRepoEVD
 
+sudo rm -r EVD-Prototype
+
+ssh-keyscan -H github.com >> ~/.ssh/known_hosts
+
 ssh-add
 ssh-add -l
 eval `ssh-agent -s`
 git clone git+ssh://git@github.com/sourav1547/EVD-Prototype.git
 
 cd EVD-Prototype
-git checkout origin/evd1
+git checkout origin/evd2
 make clean
 #echo "making code************************************"
 make
--- initGenesis.sh
+++ initGenesis.sh
@@ -5,12 +5,21 @@ SERVER_LIST=ipList
 sh staticJsonRead.sh > static.txt
 sh addressRead.sh > address.txt
 
-#echo "************** here2"
+echo "************** here2"
 python buildGenesis.py > genesis.json
 
-#echo "************** here1"
+python formStaticJson.py 
+echo "************** here1"
+var=0
 while read REMOTE_SERVER
 do
-        scp -i quorum2.key genesis.json ubuntu@$REMOTE_SERVER:/home/ubuntu/gitRepoEVD/
-        ssh -n -i quorum2.key ubuntu@$REMOTE_SERVER "geth --datadir /home/ubuntu/gitRepoEVD/.ethereum init /home/ubuntu/gitRepoEVD/genesis.json"
+	#echo staticJsonFiles/static.json$var 	
+	scp -i quorum2.key staticJsonFiles/static.json$var ubuntu@$REMOTE_SERVER:/home/ubuntu/gitRepoEVD/.ethereum/static-nodes.json
+	scp -i quorum2.key genesis.json ubuntu@$REMOTE_SERVER:/home/ubuntu/gitRepoEVD/
+	ssh -n -i quorum2.key ubuntu@$REMOTE_SERVER "nohup geth --datadir /home/ubuntu/gitRepoEVD/.ethereum  init /home/ubuntu/gitRepoEVD/genesis.json"
+	echo "processing "$REMOTE_SERVER
+	ssh -n -i quorum2.key ubuntu@$REMOTE_SERVER "killall geth; nohup geth --datadir /home/ubuntu/gitRepoEVD/.ethereum --rpc --rpcport 22001 --port 21000 --verbosity 4 --hashpower 7.1 --k 5 --allow-insecure-unlock --unlock 0 --password /home/ubuntu/gitRepoEVD/passwords.txt 2>>1.log &" &
+	var=$((var+1))
 done < $SERVER_LIST
+
+#sh insertDelay.sh
